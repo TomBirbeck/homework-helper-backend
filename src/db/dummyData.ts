da5bf9add5611f2c0002bee4d43bf5c15e187137
unknown <tombirbeck47@gmail.com>
2022-12-01 13:52:02 +0000

    inital set up

---
+++ src/db/dummyData.ts
@@ -0,0 +1,35 @@
+const list = [
+    {subject: "maths",
+    topic: "pythagoras",
+    due: "12-12-2022",
+    description: "test",
+    completed: false
+},
+    {subject: "maths",
+    topic: "algebra",
+    due: "15-12-2022",
+    description: "test",
+    completed: false
+},
+    {subject: "biology",
+    topic: "photosythensis",
+    due: "20-12-2022",
+    description: "test",
+    completed: false
+},
+    {subject: "history",
+    topic: "english civil war",
+    due: "12-12-2022",
+    description: "test",
+    completed: false
+},
+    {subject: "it",
+    topic: "databases",
+    due: "17-12-2022",
+    description: "test",
+    completed: false
+},
+
+]
+
+export default list
