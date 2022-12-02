6a0fabc2f5c4709806283021f63d61909c51fcf0
unknown <tombirbeck47@gmail.com>
2022-12-02 00:30:16 +0000

    set up db index file, basic server and routes

---
+++ src/db/index.ts
@@ -0,0 +1,9 @@
+import 'dotenv/config';
+import pg from 'pg';
+
+export default new pg.Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  });
---
+++ src/server/routes.ts
@@ -0,0 +1,30 @@
+import express from "express";
+const router = express.Router()
+
+router.get('/', async (req, res, next) =>{
+    
+})
+
+router.get('/signup', async (req, res,next) =>{
+    
+})
+
+router.get('/student', async (req, res,next) =>{
+    
+})
+
+router.get('/parent', async (req, res, next) =>{
+    
+})
+
+router.post('/student', async (req, res, next) =>{
+
+})
+
+router.patch('/student/:id', async (req, res, next) =>{
+
+})
+
+router.delete('/student/:id', async (req, res, next) =>{
+
+})
