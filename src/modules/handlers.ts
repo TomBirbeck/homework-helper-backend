6fc947db547bc2a70ddae4c80fc7e9dcfc8c388b
unknown <tombirbeck47@gmail.com>
2022-12-04 13:57:15 +0000

    server now runs, built out routes for posting student, parent and tasks. need to get the creator email and attach to task object

--- src/modules/handlers.ts
+++ src/modules/handlers.ts
@@ -1,13 +1,23 @@
-import pool from '../db/index'
+import pool from '../db/index.js'
+
+export const test = async () => {
+    const res = await pool.query(`SELECT * FROM student;`)
+    return res.rows
+    }
 
 export const createNewStudent = async (firstname:String, surname:String, email:String, password:String) =>{
     const code = `${firstname}` + `${surname}`
 const res = await pool.query('INSERT INTO student (firstname, surname, email, password, code) VALUES ($1,$2,$3,$4,$5) RETURNING *;', [firstname, surname, email, password, code])
+return res.rows
 }
+
 export const createNewParent = async (firstname:String, surname:String, email:String, password:String, childId:String) =>{
     const res = await pool.query('INSERT INTO student (firstname, surname, email, password, child_id) VALUES ($1,$2,$3,$4,$5) RETURNING *;', [firstname, surname, email, password, childId])
+    return res.rows
 }
-export const createNewTask = (subject:String, topic:String, description:String, due:String, completed:Boolean, creator_email:String) =>{
+export const createNewTask = async (subject:String, topic:String, description:String, due:String, completed:Boolean,) =>{
+    const res = await pool.query('INSERT INTO tasks (subject, topic, description, due, completed) VALUES ($1,$2,$3,$4,$5) RETURNING*;', [subject, topic, description, due, completed])
+    return res.rows
 }
 export const getTasksForStudent = (studentId:Number) => {
 }
--- src/server/routes.ts
+++ src/server/routes.ts
@@ -1,14 +1,33 @@
 import express from "express";
+import { createNewParent, createNewStudent, createNewTask, test } from "../modules/handlers.js";
 const router = express.Router()
 
-router.get('/home', async (req, res, next) =>{
-    res.json({message: "welcome to the home page"})
+router.get('/', async (req, res, next) =>{
+    try {
+        res.json({message: "welcome to the home page"})
+    } catch (error) {
+        res.status(404)
+        res.json({message: "Error 404 page not found"})
+    }
     next()
 })
 
-// router.post('/signup', async (req, res,next) =>{
+router.get('/test',async (req, res, next) => {
+    try {
+        const payload = await test()
+        console.log(payload)
+        res.send({message: "here is your test data", payload: payload})
+    } catch (error) {
+        res.status(400)
+        res.json({message:"test route not working"})
+    }
+    next()
     
-// })
+})
+
+router.post('/signup', async (req, res,next) => {
+    
+})
 
 router.get('/student', async (req, res,next) =>{
     
@@ -19,7 +38,37 @@ router.get('/parent', async (req, res, next) =>{
 })
 
 router.post('/student', async (req, res, next) =>{
-
+    try { 
+        const {firstname, surname, email, password} = req.body
+        const payload = await createNewStudent(firstname, surname, email, password)
+        res.json({message: "new user created", payload: payload})
+    } catch (error) {
+        res.status(404)
+        res.json({message: "error in creating student"})
+    }
+    next()
+})
+router.post('/parent', async (req, res, next) =>{
+    try { 
+        const {firstname, surname, email, password, child_id} = req.body
+        const payload = await createNewParent(firstname, surname, email, password, child_id)
+        res.json({message: "new parent created", payload: payload})
+    } catch (error) {
+        res.status(404)
+        res.json({message: "error in creating parent"})
+    }
+    next()
+})
+router.post('/tasks', async (req, res, next) =>{
+    try { 
+        const {subject, topic, description, due, completed} = req.body
+        const payload = await createNewTask(subject, topic, description, due, completed)
+        res.json({message: "new task created", payload: payload})
+    } catch (error) {
+        res.status(404)
+        res.json({message: "error in creating task"})
+    }
+    next()
 })
 
 router.patch('/student/:id', async (req, res, next) =>{
