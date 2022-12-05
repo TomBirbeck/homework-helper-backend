d82c2417965182d3a421bab4c42d38a086d091c6
unknown <tombirbeck47@gmail.com>
2022-12-05 23:54:02 +0000

    cors types added for ts

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import useRouter from './routes.js'
+import cors from 'cors'
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(cors())
 app.use(express.json());
 
 app.use('/', useRouter);
