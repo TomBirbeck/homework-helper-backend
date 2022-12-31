6e2477238316b75e85ad76f241c0636ebc3af814
unknown <tombirbeck47@gmail.com>
2022-12-31 01:16:31 +0000

    added test for deleting student

--- src/modules/__tests__/handler.test.ts
+++ src/modules/__tests__/handler.test.ts
@@ -1,4 +1,4 @@
-import { getAllStudents, getStudentByEmail, getStudentById } from "../handlers"
+import { createNewStudent, deleteStudent, getAllStudents, getStudentByEmail, getStudentById, updateStudent } from "../handlers"
 // import pool from '../../db/index'
 
 // beforeEach(() => {
@@ -34,5 +34,25 @@ test('get student by email',async () => {
         email: "demo@email.com",
         student_code: expect.any(String)
     })])
+    expect(expected).toMatchObject(actual)
+})
+
+test('create student',async () => {
+    const createStudentFunction = jest.fn(createNewStudent)
+    const actual = await createStudentFunction("John", "Test", "test@email.com")
+    const expected = expect.arrayContaining([expect.objectContaining({
+        student_id: expect.any(Number),
+        firstname: "John",
+        surname: "Test",
+        email: "test@email.com",
+        student_code: expect.any(String)
+    })])
+    expect(expected).toMatchObject(actual)
+})
+
+test('delete student',async () => {
+    const deleteStudentFunction = jest.fn(deleteStudent)
+    const actual = await deleteStudentFunction(6)
+    const expected: Array<''> = []
     expect(expected).toMatchObject(actual)
 })
