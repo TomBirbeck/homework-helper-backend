f3b6c1c9d31ad6ba5ec87632f77751420129e0d6
unknown <tombirbeck47@gmail.com>
2023-01-01 17:51:49 +0000

    tasks testing done

--- src/modules/__tests__/handler.test.ts
+++ src/modules/__tests__/handler.test.ts
@@ -1,4 +1,4 @@
-import { createNewParent, createNewStudent, deleteParent, deleteStudent, getAllStudents, getParentByEmail, getParentById, getStudentByEmail, getStudentById, updateStudent } from "../handlers"
+import { completeTask, createNewParent, createNewStudent, createNewTask, deleteParent, deleteStudent, deleteTask, getAllStudents, getParentByEmail, getParentById, getStudentByEmail, getStudentById, getStudentTasksForParent, getTasksForStudent, updateStudent } from "../handlers"
 // import pool from '../../db/index'
 
 // beforeEach(() => {
@@ -107,5 +107,68 @@ test('delete parent',async () => {
     const expected: Array<''> = []
     expect(expected).toMatchObject(actual)
 })
+
+})
+
+describe('tests for tasks', () => {
+    test('create task', async () => {
+        const actual = await createNewTask("Maths", "Algebra", "", "20/07/2023", false, '68efc6c6-926b-4cd5-bbe2-e05b76f66ebb')
+        const expected = [{
+            task_id: expect.any(Number),
+            subject: "Maths",
+            topic: "Algebra",
+            description: "",
+            due: "20/07/2023",
+            completed: false,
+            creator_id: '68efc6c6-926b-4cd5-bbe2-e05b76f66ebb'
+        }]
+        expect(expected).toStrictEqual(actual)
+    })
+
+     test('get tasks for student', async () => {
+        const actual = await getTasksForStudent('68efc6c6-926b-4cd5-bbe2-e05b76f66ebb')
+        const expected = expect.arrayContaining([{
+            task_id: expect.any(Number),
+            subject: expect.any(String),
+            topic: expect.any(String),
+            description: expect.any(String),
+            due: expect.any(String),
+            completed: expect.any(Boolean),
+            creator_id: '68efc6c6-926b-4cd5-bbe2-e05b76f66ebb'
+        }])
+        expect(expected).toStrictEqual(actual)
+    })
+     test('get tasks for parent', async () => {
+        const actual = await getStudentTasksForParent('68efc6c6-926b-4cd5-bbe2-e05b76f66ebb')
+        const expected = expect.arrayContaining([{
+            task_id: expect.any(Number),
+            subject: expect.any(String),
+            topic: expect.any(String),
+            description: expect.any(String),
+            due: expect.any(String),
+            completed: expect.any(Boolean),
+            creator_id: '68efc6c6-926b-4cd5-bbe2-e05b76f66ebb'
+        }])
+        expect(expected).toStrictEqual(actual)
+    })
+     test('complete task (will actually toggle between)', async () => {
+        const actual = await completeTask(13)
+        const expected = [{
+            task_id: 13,
+            subject: expect.any(String),
+            topic: expect.any(String),
+            description: expect.any(String),
+            due: expect.any(String),
+            completed: true,
+            creator_id: expect.any(String)
+        }]
+        expect(expected).toStrictEqual(actual)
+    })
+
+    test('delete task', async () => {
+        const actual = await deleteTask(12)
+        const expected: Array<''> = []
+        expect(expected).toStrictEqual(actual)
+    })
 
 })
