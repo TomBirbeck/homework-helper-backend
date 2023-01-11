2bc733446a2de3ab8f20a114ec24f7c3bb5a2503
unknown <tombirbeck47@gmail.com>
2023-01-11 17:20:22 +0000

    fixed?

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import useRouter from './routes'
+import useRouter from './routes.js'
 import cors from 'cors'
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -13,8 +13,6 @@ app.use('/', useRouter);
 //     res.json({ message: 'hello' });
 //     next()
 // });
-
-// app.use('/homework', useRouter);
 
 app.listen(PORT, async () => console.log(`app listening on port ${PORT}`));
 
