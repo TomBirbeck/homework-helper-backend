c0ba196aaf94255c1407b50fd9e2ab7d6dc400c6
unknown <tombirbeck47@gmail.com>
2023-02-06 08:20:46 +0000

    added new column to database, set up handler and route for shadow deleting

--- src/db/scripts/addColumn.ts
+++ src/db/scripts/addColumn.ts
@@ -1,3 +1,3 @@
 import pool from '../index'
 
-await pool.query("ALTER TABLE tasks ADD COLUMN priority VARCHAR(20) DEFAULT 'low'")
+await pool.query("ALTER TABLE tasks ADD COLUMN deleted Boolean DEFAULT false")
